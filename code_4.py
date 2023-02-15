184a55a64f3f260eea133740a26ff6ac74e6d0ff
Litzy Melo <litzymelo@yahoo.com>
2023-02-15 16:24:28 -0700

    new2

--- code_4.py
+++ code_4.py
@@ -5,14 +5,17 @@ big_mac_file = './big-mac-full-index.csv'
 def get_big_mac_price_by_year(year,country_code):
     country_code = country_code.upper()
     df = pd.read_csv(big_mac_file)
-   query(f'{iso_a3},{year}01-01 => {year}12-31')
+    query=(f"{iso_a3} == '{year}'and date > {year}-01-01 and date < {year}-12-31")
     return result
 
 
     
 
 def get_big_mac_price_by_country(country_code):
-    country_code = country_code.upper
+    country_code = country_code.upper()
+    df = pd.read_csv(big_mac_file)
+    query(f'{})
+    
 
     
 
@@ -28,5 +31,5 @@ def get_the_most_expensive_big_mac_price_by_year(year):
 
     
 if __name__ == "__main__":
-    print(country_code)
+    
     
