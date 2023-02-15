9b345d6763dda2dd10822202e281c142aba4f7ab
Litzy Melo <litzymelo@yahoo.com>
2023-02-15 16:09:08 -0700

    m

--- code_4.py
+++ code_4.py
@@ -3,16 +3,31 @@ import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    country_code = country_code.upper
+    df = pd.read_csv(big_mac_file)
+    (df'[iso_a3] + [year]01-01 = [year]12-01')
+                         
+    return result
+
+
+    
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    country_code = country_code.upper
+
+    
+
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    
+  
+    
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+   
 
+
+    
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    print(country_code)
+    
